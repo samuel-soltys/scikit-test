9b9be152365c8f2cb39429bf1821276dfc6adbb2
Samuel Šoltys <samuelsasanka@gmail.com>
2023-07-03 15:29:30 +0200

    K means clustering

--- k-means-clustering/k-means-clustering.py
+++
@@ -1,7 +0,0 @@
-# Kmeans clustering 
-# unsupervised learning model (clustering)
-# clusters the data to labels
-# using Centroids - two random points, then a line (hyperplane) in between them,
-#   and then an average from X axis and Y axis of the pointso n either side,
-#    and moving the two points to its average locatoin from the points on their side
-
